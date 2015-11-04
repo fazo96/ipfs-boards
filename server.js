4501fdfdafb3936e8f30e0491ef09aa75f543796
Enrico Fasoli <fazius2009@gmail.com>
2015-11-04 16:41:17 +0100

    implement http server

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 #!/usr/bin/env node
 
 var ipfs = require('ipfs-api')('localhost','5001')
+var express = require('express')
+var app = express()
 
+// Serve files in ./static
+app.use(express.static('static'))
 
+// CatchAll route: serve the angular app
+app.get('*',function(req,res){
+  res.sendFile(__dirname+'/static/index.html')
+})
+
+// Start http server
+app.listen(3000,function(){
+  console.log('Started')
+})
