49e8a75e3de54f2c9929590c59214a8ec2f65411
Enrico Fasoli <fazius2009@gmail.com>
2015-11-11 12:38:10 +0100

    user detection works finally

--- server/server.js
+++ server/server.js
@@ -10,7 +10,39 @@ var boards = new BoardsAPI(ipfs)
 // Serve web app
 app.use(express.static('../webapp'))
 
+// Generates a callback function to pass the API response to HTTP
+var apiToHandler = function(request,response){
+  return (function(a,b){
+    var req = a
+    var res = b
+    return function(err,r){
+      if(err){
+        res.status(500)
+        res.send(err)
+      } else {
+        if(r.split){
+          res.send(r)
+        } else {
+          res.json(r)
+        }
+      }
+    }
+  })(request,response)
+}
+
 // Create gateways to access the BoardsAPI
+
+app.get('/@:user',(req,res) => {
+  boards.getProfile(req.params.user,apiToHandler(req,res))
+})
+
+app.get('/@:user/:board',(req,res) => {
+  boards.getBoardSettings(req.params.user,req.params.board,apiToHandler(req,res))
+})
+
+//boards.searchUsers()
+
+// Start the web server
 
 function startWebServer(){
   // Start http server
