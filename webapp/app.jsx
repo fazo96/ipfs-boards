46c2eb43095aa5173d1aebace386bb4df6933623
Enrico Fasoli <fazius2009@gmail.com>
2015-11-14 13:12:48 +0100

    added routing and some css

--- webapp/app.jsx
+++ webapp/app.jsx
@@ -1,5 +1,10 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
+var Router = require('react-router').Router
+var Route = require('react-router').Route
+var IndexRoute = require('react-router').IndexRoute
+var Link = require('react-router').Link
+
 var ipfs = require('ipfs-api')('localhost',5001)
 var BoardsAPI = require('../lib/boards-api.js')
 
@@ -20,7 +25,11 @@ var App = React.createClass({
 var Homepage = React.createClass({
   render: function(){
     return (
-      <h3>Hello</h3>
+      <div>
+        <h3>Hello</h3>
+        <p>Not much is implemented...</p>
+        <p>You can try <Link to="@QmXnfA41SXMX3tqFD4kjED7ehyvgTsuAho86TkEoTbZdpw">Opening a Profile</Link> though :)</p>
+      </div>
     )
   }
 })
@@ -30,7 +39,7 @@ var Navbar = React.createClass({
     return (
       <div className="navbar">
         <div className="container">
-          <h4>Boards</h4>
+          <h4><Link to="/">Boards</Link></h4>
         </div>
       </div>)
   }
@@ -41,7 +50,7 @@ var Profile = React.createClass({
     return { name: '...' }
   },
   componentDidMount: function(){
-    boards.getProfile(this.props.id, (err,res) => {
+    boards.getProfile(this.props.params.userid, (err,res) => {
       if(err){
         console.log(err)
         this.setState({
@@ -58,11 +67,27 @@ var Profile = React.createClass({
     return (<div className="profile">
       <h1>{this.state.name}</h1>
       <p>{this.state.error}</p>
-      <h5 className="light">@{this.props.id}</h5>
+      <h5 className="light">@{this.props.params.userid}</h5>
     </div>)
   }
 })
 
+var Board = React.createClass({
+  getInitialState: function(){
+    return { posts: [] }
+  },
+  render: function(){
+    return (
+      <div></div>
+    )
+  }
+})
+
 ReactDOM.render(
-  <App><Profile id="QmXnfA41SXMX3tqFD4kjED7ehyvgTsuAho86TkEoTbZdpw"/></App>, document.getElementById('root')
+  <Router>
+    <Route path="/" component={App}>
+      <IndexRoute component={Homepage} />
+      <Route path="/@:userid" component={Profile} />
+    </Route>
+  </Router>, document.getElementById('root')
 )
