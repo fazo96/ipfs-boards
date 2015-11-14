ffac0c8f6588a746961a937f90b456f95098735e
Enrico Fasoli <fazius2009@gmail.com>
2015-11-14 12:06:25 +0100

    script to set CORS settings on your ipfs daemon

---
+++ ipfs_daemon_set_cors.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "GET", "POST"]'
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Credentials '["true"]'
