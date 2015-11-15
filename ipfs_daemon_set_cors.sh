90dbd64265a94381a5b13603dc7e67325e70a429
Enrico Fasoli <fazius2009@gmail.com>
2015-11-15 21:35:50 +0100

    documented the cors script a bit

--- ipfs_daemon_set_cors.sh
+++ ipfs_daemon_set_cors.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This will make sure your ipfs node works with the app.
+# Note that it's not safe to do if your node is accessible from the internet or your LAN. Be careful!
+
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "GET", "POST"]'
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Credentials '["true"]'
