657a18513d63cc3a8c1d3a51684ab088f886bdf8
Enrico Fasoli <fazius2009@gmail.com>
2015-11-18 17:03:52 +0100

    updated cors istructions

--- ipfs_daemon_set_cors.sh
+++ ipfs_daemon_set_cors.sh
@@ -2,7 +2,11 @@
 
 # This will make sure your ipfs node works with the app.
 # NOTE: this change is PERMANENT until reversed!
-# See https://github.com/ipfs/js-ipfs-api#cors for a temporary solution instead
+
+# If you want a temporary solution, try running your daemon like this:
+# API_ORIGIN="localhost:8080" ipfs daemon
+# replace localhost:8080 with whatever domain:port you're connecting from :)
+# Also see https://github.com/ipfs/js-ipfs-api#cors
 
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "GET", "POST"]'
