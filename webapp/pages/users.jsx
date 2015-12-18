d53ce31b514ff494b4a06ae224391ac6187c7f1b
Enrico Fasoli <fazius2009@gmail.com>
2015-12-18 22:32:15 +0100

    another refactor, also closes #45

--- webapp/pages/users.jsx
+++ webapp/pages/users.jsx
@@ -5,24 +5,13 @@ var UserID = require('userID.jsx')
 
 module.exports = function (boardsAPI) {
   return React.createClass({
-    getInitialState: function () {
+    getInitialState () {
       return { users: [], api: false }
     },
-    componentDidMount: function () {
+    componentDidMount () {
       boardsAPI.use(boards => {
-        boards.init()
-        if (boards.isInit) {
-          if (this.isMounted()) {
-            this.init(boards)
-          }
-        }
-        var ee = boards.getEventEmitter()
-        ee.on('init', e => {
-          if (!e && this.isMounted()) {
-            this.init(boards)
-          }
-        })
-        ee.on('user', (id) => {
+        this.init(boards)
+        boards.getEventEmitter().on('user', (id) => {
           if (id === undefined || id === 'undefined') console.log('found undefined user???')
           if (this.isMounted() && this.state.users.indexOf(id) < 0) {
             this.setState({ users: this.state.users.concat(id) })
@@ -30,24 +19,22 @@ module.exports = function (boardsAPI) {
         })
       })
     },
-    init: function (boards) {
-      if (this.isMounted() && !this.state.init) {
-        this.setState({ users: boards.getUsers(), api: true, init: true, boards: boards })
-        boards.searchUsers()
-      }
+    init (boards) {
+      this.setState({ users: boards.getUsers(), api: boards })
+      boards.searchUsers()
     },
-    render: function () {
+    render () {
       if (this.state.api) {
         return <div>
-          <h1><Icon name="users" /> Users</h1>
+          <h1><Icon name="users" className="light" /> Users</h1>
           <p>Found <b>{this.state.users.length}</b> users</p>
           <ul>
             {this.state.users.map(user => {
-              return <UserID key={user} id={user} api={this.state.boards} />
+              return <UserID key={user} id={user} api={this.state.api} />
             })}
           </ul>
         </div>
-      } else return <GetIPFS api={this.state.boards} />
+      } else return <GetIPFS api={this.state.api} />
     }
   })
 }
